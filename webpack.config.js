4a06b07c165d07408e11f86f57eec735cc82112a
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-07-13 18:47:11 +0300

    init: start level2 task. Create structure for project and add webpack in project. Create .gitignore and add rules

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const pathSrc = path.resolve(__dirname, 'src');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: {
+          app: {
+            name: 'chrome'
+          }
+        },
+        host: 'localhost',
+        watchFiles: [
+          `${pathSrc}/**/*.html`,
+          `${pathSrc}/img/**/*.*`
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, 'src/favicon.ico'),
+              to: path.resolve(__dirname, 'dist'),
+              noErrorOnMissing: true
+            },
+            {
+              from: path.resolve(__dirname, 'src/img'),
+              to: path.resolve(__dirname, 'dist/img'),
+              noErrorOnMissing: true,
+              force: true
+            }
+          ]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        config.plugins.push(new MiniCssExtractPlugin());
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
